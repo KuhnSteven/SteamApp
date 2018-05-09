39714d2d84050424f16aab71f65e5c83fbe5306f
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-05-09 17:09:45 -0400

    Gulp/sass setup

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const browserSync = require('browser-sync').create();
+
+gulp.task('default', function () {
+
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+        stream:true
+    }));
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
